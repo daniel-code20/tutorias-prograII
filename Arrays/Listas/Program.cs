c3b51ba8f88c59cb1cf63f90335e7058b5c3f8be
daniel-code20 <dgithub23@gmail.com>
2024-09-17 09:09:42 -0600

    LinQ

---
+++ Arrays/Listas/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+namespace MiNuevoPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crea un programa que permita al usuario agregar nombres a una lista hasta que
+            // ingrese "salir". Luego imprime todos los nombres ingresados.
+
+            List<string> nombres = new List<string>();
+            string nombre;
+
+            do
+            {
+                Console.WriteLine("Ingrese un nombre o escriba 'salir' para terminar");
+                nombre = Console.ReadLine();
+
+                if(nombre.ToLower() != "salir")
+                {
+                    nombres.Add(nombre);
+                }
+
+            } while (nombre.ToLower() != "salir");
+
+            Console.WriteLine("Nombres Ingresados:");
+
+            foreach (string n in nombres)
+            {
+                Console.WriteLine(n);
+            }
+
+
+
+        }
+    }
+}
---
+++ LinQ/LinQ/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiNuevoPrograma
+{
+    class Estudiante
+    {
+        public string Nombre { get; set; }
+
+        public int Calificacion { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Descripción: Crea una clase Estudiante, luego usa LINQ para filtrar
+            //estudiantes con una calificación mayor a 85 y ordénalos por su nombre.
+
+            List<Estudiante> estudiantes = new List<Estudiante>
+            {
+                new Estudiante {Nombre = "Mauricio", Calificacion = 90},
+                new Estudiante {Nombre = "Luis", Calificacion = 88},
+                new Estudiante {Nombre = "Bryan", Calificacion = 84},
+                new Estudiante {Nombre = "Ivan", Calificacion = 70},
+            };
+
+            /*var estudianteFiltrados = estudiantes.Where(e => e.Calificacion < 85)
+                .OrderBy(e => e.Nombre);*/
+
+            var estudianteFiltrados = from e in estudiantes
+                                      where e.Calificacion < 85
+                                      orderby e.Nombre
+                                      select e;
+
+            Console.WriteLine("Estudiantes con nota mayor a 85");
+
+            foreach (var item in estudianteFiltrados)
+            {
+                Console.WriteLine($"{item.Nombre}: {item.Calificacion}");
+            }
+
+        }
+    }
+}
